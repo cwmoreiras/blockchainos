877a117472ee57f764d11c53ec01dc7ace98acfd
cwmoreiras <cwmoreiras@gmail.com>
2019-10-10 18:03:30 -0700

    tested merge

--- blockchain/src/blockchain.c
+++ blockchain/src/blockchain.c
@@ -190,8 +190,8 @@ void blockframe_print(uint8_t *this)
     &index, &timestamp, &record_sz, record);
 
   printf("-------------------------------------------------------------------------\n");
-  util_buf_print_hex(prevhash, SHA256_DIGEST_LENGTH, "phash", 1);
-  util_buf_print_hex(hash, SHA256_DIGEST_LENGTH, "hash ", 1);
+  util_buf_print_hex(prevhash, HASH_SZ, "phash", 1);
+  util_buf_print_hex(hash, HASH_SZ, "hash ", 1);
 
   printf("index: %lu\n", index);
   printf("tstmp: %lu\n", timestamp);
